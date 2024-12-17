27734049ccb9c0517d75f47c86055baab5d688d0
GitEmanuel24 <emanueldj2010@hotmail.com>
2024-12-16 22:30:19 -0300

    Primeiro Upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Adiciona um scroll suave para os links do menu
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
